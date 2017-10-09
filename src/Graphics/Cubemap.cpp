568473b7c9f39b51b4033f6c6c8807d126ec9453
Fiona Mallett <Fiona Mallett>
2017-10-09 19:21:25 +1000

    Cubemap files updated

--- src/Graphics/Cubemap.cpp
+++ src/Graphics/Cubemap.cpp
@@ -17,47 +17,72 @@ Cubemap::Cubemap() : mData(nullptr)
 
 }
 
-Cubemap::Cubemap(const std::string &filename, bool gamma) :
+Cubemap::Cubemap(std::vector<std::string> textures_faces, bool gamma) :
 	mData(nullptr)
 {
-	LoadFromFile(filename, gamma);
+	LoadFromFile(textures_faces, gamma);
 }
 
-void Cubemap::LoadFromFile(const std::string &filename, bool gamma)
+unsigned int Cubemap::LoadFromFile(std::vector<std::string> textures_faces, bool gamma)
 {
-	mFilename = filename;
 
-	int width, height, nrComponents;
-	mData = stbi_load(filename.c_str(), &width, &height, &nrComponents, 0);
+	int width, height, nrChannels;
+	for (unsigned int i = 0; i < textures_faces.size(); i++)
+	{
+		unsigned char *data = stbi_load(textures_faces[i].c_str(), &width, &height, &nrChannels, 0);
+		if (data)
+		{
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+				0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
+			);
+			stbi_image_free(data);
+		}
+		else
+		{
+			std::cout << "Cubemap texture failed to load at path: " << textures_faces[i] << std::endl;
+			stbi_image_free(data);
+		}
+
+	}
 
 	if (mData != nullptr)
 	{
 		GLenum format;
-		if (nrComponents == 1)
+		if (nrChannels == 1)
 			format = GL_RED;
-		else if (nrComponents == 3)
+		else if (nrChannels == 3)
 			format = GL_RGB;
-		else if (nrComponents == 4)
+		else if (nrChannels == 4)
 			format = GL_RGBA;
 
 		glGenTextures(1, &mCubemapID);
-		glBindTexture(GL_TEXTURE_2D, mCubemapID);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, mCubemapID);
 		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, mData);
 		glGenerateMipmap(GL_TEXTURE_2D);
 
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 		stbi_image_free(mData);
 	}
-	else
-	{
-		std::cout << "Cubemap failed to load: " << filename << std::endl;
-	}
+	return mCubemapID;
 }
 
+void loadSkyboxFaces() {
+	// load textures
+	std::vector<std::string> faces
+	{
+		("bin/assets/textures/skybox/right.jpg"),
+		("bin/assets/textures/skybox/left.jpg"),
+		("bin/assets/textures/skybox/top.jpg"),
+		("bin/assets/textures/skybox/bottom.jpg"),
+		("bin/assets/textures/skybox/back.jpg"),
+		("bin/assets/textures/skybox/front.jpg")
+	};
+}
 int Cubemap::GetCubemapID() const
 {
 	return mCubemapID;
@@ -66,5 +91,5 @@ int Cubemap::GetCubemapID() const
 void Cubemap::Bind(unsigned int cubemapSlot)
 {
 	glActiveTexture(GL_TEXTURE0 + cubemapSlot);
-	glBindTexture(GL_TEXTURE_2D, mCubemapID);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, mCubemapID);
 }
--- src/Graphics/Cubemap.h
+++ src/Graphics/Cubemap.h
@@ -2,19 +2,20 @@
 #define CUBEMAP_H
 
 #include <string>
+#include <vector>
 
 class Cubemap
 {
 public:
 	Cubemap();
-	Cubemap(const std::string &filename, bool gamma);
+	Cubemap(std::vector<std::string> textures_faces, bool gamma);
 
-	void LoadFromFile(const std::string &filename, bool gamma);
+	unsigned int LoadFromFile(std::vector<std::string> textures_faces, bool gamma);
 	int GetCubemapID() const;
 	void Bind(unsigned int cubemapSlot);
 
 private:
-	std::string mFilename;
+	std::vector<std::string> mTexture_faces;
 
 	unsigned char *mData;
 	unsigned int mCubemapID;
