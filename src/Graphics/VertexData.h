34429baddedf2c0f8da3b5554eccad60a5b9996b
Nathan Dos <creativitynlogic@gmail.com>
2017-10-27 04:59:06 +1100

    Added terrain and train bridge.
    Signed-off-by: Nathan Dos <creativitynlogic@gmail.com>

--- src/Graphics/VertexData.h
+++ src/Graphics/VertexData.h
@@ -1,8 +1,7 @@
 #ifndef VERTEXDATA_H
 #define VERTEXDATA_H
 
-#include "glm/vec3.hpp"
-#include "glm/vec2.hpp"
+#include <glm/glm.hpp>
 
 #define NUM_BONES_PER_VEREX 4
 
@@ -16,12 +15,6 @@ struct Vertex
 	glm::vec3 Colour;
 	glm::ivec4 BoneIDs;
 	glm::vec4 VertexWeights;
-};
-
-struct BoneInfo
-{
-	glm::mat4 BoneOffset;
-	glm::mat4 FinalTransformation;
 };
 
 #endif // VERTEXDATA_H
